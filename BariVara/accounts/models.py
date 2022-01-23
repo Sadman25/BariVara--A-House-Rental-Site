8842548dbf2f759d70d7432c80a1e95c0be3ceff
Sadman25 <shadman.saqif@northsouth.edu>
2022-01-23 20:16:29 +0600

    #2 Implemented Backend of user login/registration and logout

--- BariVara/accounts/models.py
+++ BariVara/accounts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.urls import reverse
+from django.contrib.auth.models import User
 # Create your models here.
+
+class profile (models.Model):
+    user = models.OneToOneField(User,null=False,blank=False, on_delete=models.CASCADE)
+    profile_picture = models.ImageField(null=False,blank=False)
+
+
+    def __str__(self) -> str:
+        return self.user.username
--- BariVara/accounts/views.py
+++ BariVara/accounts/views.py
@@ -2,9 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-
-#from .forms import userRegistration,profileRegistration
-
+from .forms import userRegistration,profileRegistration
 # Create your views here.
 
 def loginPage(request):
@@ -19,13 +17,31 @@ def loginPage(request):
         else:
             messages.info (request, 'Email address or Password is incorrect')
             
-    
     return render (request,'loginpage.html')
 
 def registration(request):
-    
-    return render (request,'registration.html')
+    userForm = userRegistration()
+    profileForm = profileRegistration()
 
+    if request.method == 'POST':
+        userForm = userRegistration(request.POST)
+        profileForm = profileRegistration(request.POST,request.FILES)
+        if userForm.is_valid() and profileForm.is_valid():
+            user = userForm.save()
+            profile = profileForm.save(commit=False)
+            profile.user = user
+            profile.save()
+            messages.success(request,'Account was created for ' + profile.user.username)
+            return redirect ('loginPage')
+
+    else:
+        print(userForm.errors, profileForm.errors)
+    
+    context = {
+        'userForm':userForm,
+        'profileForm':profileForm
+    }
+    return render (request,'registration.html',context)
 
 def logoutUser(request):
     logout(request)
