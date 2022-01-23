b4b3c7ae60e1df7cadcb409f328abc33fda77257
Sadman25 <shadman.saqif@northsouth.edu>
2022-01-23 20:09:42 +0600

    #2 Urls and views connected with the frontend

--- BariVara/accounts/views.py
+++ BariVara/accounts/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+#from .forms import userRegistration,profileRegistration
 
 # Create your views here.
+
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username = username, password = password)
+        if user is not None:
+            login(request, user)
+            return redirect ('homePage')
+        else:
+            messages.info (request, 'Email address or Password is incorrect')
+            
+    
+    return render (request,'loginpage.html')
+
+def registration(request):
+    
+    return render (request,'registration.html')
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('loginPage')
