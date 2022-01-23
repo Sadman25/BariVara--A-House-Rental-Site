83a06138b01c2b68aeffac67271e29d1a3bb19e9
Sadman25 <shadman.saqif@northsouth.edu>
2022-01-23 19:14:43 +0600

    #1 Django Project created

---
+++ BariVara/BariVara/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for BariVara project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BariVara.settings')
+
+application = get_wsgi_application()
