4857456dcbe1939fbe113dc9b3f06bf4ae276132
Sadman25 <shadman.saqif@northsouth.edu>
2022-01-24 18:57:09 +0600

    #3 house model implemented and migrated

--- BariVara/house/admin.py
+++ BariVara/house/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(area)
+admin.site.register(advertisement)
+admin.site.register(image)
+admin.site.register(comment)
--- BariVara/house/models.py
+++ BariVara/house/models.py
@@ -1,3 +1,34 @@
 from django.db import models
-
+from django.urls import reverse
+from django.contrib.auth.models import User
 # Create your models here.
+
+class area (models.Model):
+    area = models.CharField(max_length=50, unique=True)
+    
+    def __str__(self) -> str:
+        return self.area
+
+class advertisement(models.Model):
+    area = models.ForeignKey(area,null=True,on_delete= models.SET_NULL)
+    address=models.CharField(max_length=100)
+    bedroom=models.PositiveIntegerField()
+    bathroom=models.PositiveIntegerField()
+    rent=models.PositiveIntegerField()
+    size=models.PositiveIntegerField()
+    owner= models.ForeignKey(User,on_delete=models.CASCADE)
+    phone_number=models.CharField(max_length=11)
+
+class image(models.Model):
+    advertisement= models.ForeignKey(advertisement,on_delete=models.CASCADE)
+    image= models.ImageField(blank=False, null=False)
+
+    def __str__(self):
+        return self.advertisement.house_owner
+
+class comment(models.Model):
+    advertisement= models.ForeignKey(advertisement,on_delete=models.CASCADE)
+    user= models.ForeignKey(User,on_delete= models.CASCADE)
+    reply = models.ForeignKey('Comment' , null=True , related_name = "replies" , on_delete = models.CASCADE)
+    comment= models.TextField(max_length=2000)
+    time = models.DateTimeField(auto_now_add=True,null=False)
